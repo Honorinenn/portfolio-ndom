52dd62aeeb935b84ec18b5091932755446b2d4e4
Honorine Ndom <honorinendzah@gmail.com>
2022-03-11 07:53:18 -0800

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,10 +5,10 @@ import Particle from "../Particle";
 
 import impressme from "../../Assets/Projects/impressme.png";
 import pinko from "../../Assets/Projects/pinko.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import workout from "../../Assets/Projects/workout.png";
+import musictrivia from "../../Assets/Projects/musictrivia.png";
+import weather from "../../Assets/Projects/weather.png";
+import booksearch from "../../Assets/Projects/booksearch.png";
 
 function Projects() {
   return (
@@ -44,9 +44,9 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={workout}
               isBlog={false}
-              title="Editor.io"
+              title="Workout Tracker"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               link="https://github.com/soumyajit4419/Editor.io"
             />
@@ -54,9 +54,9 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={musictrivia}
               isBlog={false}
-              title="Bits-0f-C0de"
+              title="Music Trivia"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               link="https://github.com/soumyajit4419/Bits-0f-C0de"
             />
@@ -64,9 +64,9 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={weather}
               isBlog={false}
-              title="Ai For Social Good"
+              title="Weather Dashboard"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
               link="https://github.com/soumyajit4419/AI_For_Social_Good"
             />
@@ -74,9 +74,9 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={booksearch}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
+              title="API Book Search Engine"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
