11c4799763a9702e5fb36ab0d06cd38890bee6ed
Honorine Ndom <honorinendzah@gmail.com>
2022-03-13 20:15:09 -0700

    changes done on project.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -49,7 +49,7 @@ function Projects() {
               isBlog={false}
               title="Workout Tracker"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              link="https://workout-tracker-hn.herokuapp.com/"
             />
           </Col>
 
@@ -59,7 +59,7 @@ function Projects() {
               isBlog={false}
               title="Music Trivia"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              link="https://github.com/BCole37/Project-1-Music-Trivia"
             />
           </Col>
 
@@ -69,7 +69,7 @@ function Projects() {
               isBlog={false}
               title="Weather Dashboard"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              link="https://honorinenn.github.io/Weather-Dash-Board/"
             />
           </Col>
 
@@ -80,7 +80,7 @@ function Projects() {
               title="API Book Search Engine"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              link="https://book-search-engine-hn.herokuapp.com/ "
             />
           </Col>
 
@@ -91,7 +91,7 @@ function Projects() {
               title="Note Taker"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              link="https://note1t.herokuapp.com/"
             />
           </Col>
         </Row>
