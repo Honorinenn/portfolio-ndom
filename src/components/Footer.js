7d43dee0c47ef57beb696d8cbe28bb79908bbdcc
Honorine Ndom <honorinendzah@gmail.com>
2022-03-16 04:53:39 -0700

    changes done on Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Developed by Honorine Ndom Ndzah</h3>
+          <h3>Done by Honorine Ndom Ndzah</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} HNN</h3>
