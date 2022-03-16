a48606531d96d2cc7caffd24d724db8daaea927d
Honorine Ndom <honorinendzah@gmail.com>
2022-03-16 11:09:07 -0700

    changes done on About.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -35,7 +35,7 @@ function Resume() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
         <Row className="resume">
